fbbe77e02022c2ffeffd151fb5b35ac267a3328c
Rishaba Jain <babujain86@gmail.com>
2022-01-06 23:57:51 +0530

    added card2 and card3

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,11 +1,13 @@
 import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'author_card.dart';
+import 'fooderlich_theme.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
 
-  @override build(BuildContext context) {
+  @override
+  build(BuildContext context) {
     return Center(
       child: Container(
         constraints: const BoxConstraints.expand(
@@ -23,8 +25,36 @@ class Card2 extends StatelessWidget {
         ),
         child: Column(
           children: [
-            // TODO 1: add author information
-            // TODO 4: add positioned text
+            const AuthorCard(
+              authorName: 'Rishaba Jain',
+              title: 'Smoothie Connoisseur',
+              imageProvider: AssetImage('display_picture1.jpg'),
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Positioned(
+                    bottom: 16,
+                    right: 15,
+                    child: Text(
+                      'Recipe',
+                      style: FooderlichTheme.lightTextTheme.headline1,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 70,
+                    left: 16,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(
+                        'Smoothies',
+                        style: FooderlichTheme.lightTextTheme.headline1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
