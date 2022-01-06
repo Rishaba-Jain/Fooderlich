2ba70f46777aff505a9a22a751036e437739b087
Rishaba Jain <babujain86@gmail.com>
2022-01-06 12:35:44 +0530

    added card1 and card2

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Fooderlich
-This is a Flutter project called Fooderlich which is a recipe app described in Flutter Apprentice Book on raywenderlich.com
+This is a Flutter project called Fooderlich which is a recipe app described in Flutter Apprentice Book on www.raywenderlich.com
 
 Link for the book: https://www.raywenderlich.com/books/flutter-apprentice
+
 Github link for the book materials: https://github.com/raywenderlich/flta-materials
+
+Image Credits:
+phulka1.jpg https://www.mintsrecipes.com/how-to-make-roti-dough/
+smoothies1.webp https://www.mykitchentales.in/post/mango-shake-with-a-twist
---
+++ lib/card2.dart
@@ -0,0 +1,33 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Card2 extends StatelessWidget {
+  const Card2({Key? key}) : super(key: key);
+
+  @override build(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/smoothies1.webp'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+        child: Column(
+          children: [
+            // TODO 1: add author information
+            // TODO 4: add positioned text
+          ],
+        ),
+      ),
+    );
+  }
+}
